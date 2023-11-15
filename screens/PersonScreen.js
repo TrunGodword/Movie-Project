fd4cbee813120d461d4c3bccf8201639cc5f4bae
AzureZul <128357122+AzureZul@users.noreply.github.com>
2023-11-15 11:26:45 +0800

    Enkhzul PersonScreen

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -1,12 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-const PersonScreen = () => {
-  return (
-    <View>
-      <Text>PersonScreen</Text>
-    </View>
+var {width, height}=Dimensions.get('window')
+const ios = Platform.OS === 'ios'
+const verticalMargin = ios ? ":" : "my-3"
+
+export default function PersonScreen(){
+  const navigation=useNavigation()
+  const [isFavourite,toggleFavourite]=useState(false)
+  const [personMovies,setPersonMovies]=useState([1,2,3,4])
+  return(
+    <ScrollView className="bg-neutral-900 flex-1" contentContainerStyle={{paddingBottom: 20}}>
+      {/* back button */}
+      <SafeAreaView className={"z-20 w-full flex-row justify-between items-center px-4" +verticalMargin}>
+      <TouchableOpacity onPress={()=>navigation.goBack()} style={styles.background} >
+        <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>toggleFavourite(!isFavourite)}>
+        <HeartIcon size="35" color={isFavourite?theme.background:"white"}/>
+      </TouchableOpacity>
+      </SafeAreaView>
+      {/* person details*/}
+      <View>
+        <View className="flex-row justify-center"
+          style={{
+            shadowColor:"gray",
+            shadowRadius:40,
+            shadowOffset:{width:0,height:5},
+            shadowOpacity:1
+          }}>
+          <View className="items-center rounded-full overflow-hidden h-72 w-72 border-2 border-neutral-500">
+            <Image source ={require("../assets/images/castImage2.png")}
+              style={{height:height*0.43,width:width*0.74}}/>
+          </View>
+        </View>
+        <View className="mt-6">
+          <Text className="text-3xl text-white font-bold text-center">Keenu Reeves </Text>
+          <Text className="text-base text-neutral-500  text-center">London United Kingdom </Text>
+        </View>
+        <View className="mx-3 mt-6 flex-row justify-between items-center border-neutral-700 rounded-full">
+          <View className="border-r-2 border-neutral-400 items-center">
+            <Text className="text-white font-semibold">Gender </Text>
+            <Text className="text-neutral-300 text-sm">Male</Text>
+          </View>
+          <View className="border-r-2 border-neutral-400 items-center">
+            <Text className="text-white font-semibold">Birthday </Text>
+            <Text className="text-neutral-300 text-sm">1964-09-02</Text>
+          </View>
+          <View className="border-r-2 border-neutral-400 items-center">
+            <Text className="text-white font-semibold">Known for </Text>
+            <Text className="text-neutral-300 text-sm">Acting</Text>
+          </View>
+          <View className="border-r-2 border-neutral-400 items-center">
+            <Text className="text-white font-semibold">Popularly </Text>
+            <Text className="text-neutral-300 text-sm">64.23</Text>
+          </View>
+        </View>
+        <View className="my-6 mx-4 space-y-2">
+          <Text className="text-white text-lg ">Biography</Text>
+          <Text className="text-neutral-400 tracking-wide ">
+            Keenu Charles is Canadian actor. Reeves is known for his roles in Bill </Text>
+        </View>
+         {/* movies*/}
+         <MovieList title={"Movies"} hideSeeAll={true} data ={personMovies}/>
+      </View>
+    </ScrollView>
   )
 }
-
-export default PersonScreen
